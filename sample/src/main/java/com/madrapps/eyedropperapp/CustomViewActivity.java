bd1c9ffa01003ad98d17329e2c98c89f12851158
Henry <th.saravana@gmail.com>
2017-05-27 01:52:40 +0530

    Migrate to Kotlin #3

--- sample/src/main/java/com/madrapps/eyedropperapp/CustomViewActivity.java
+++ sample/src/main/java/com/madrapps/eyedropperapp/CustomViewActivity.java
@@ -3,9 +3,13 @@ package com.madrapps.eyedropperapp;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.madrapps.eyedropper.EyeDropper;
+
+import org.jetbrains.annotations.NotNull;
 
 public class CustomViewActivity extends AppCompatActivity {
 
@@ -18,10 +22,22 @@ public class CustomViewActivity extends AppCompatActivity {
 
     private void setUp() {
         final View targetView = findViewById(R.id.customView);
-        new EyeDropper(targetView, new EyeDropper.ColorSelectionListener() {
+        final EyeDropper eyeDropper = new EyeDropper(targetView, new EyeDropper.ColorSelectionListener() {
             @Override
             public void onColorSelected(@ColorInt int color) {
                 (findViewById(R.id.colorSlot)).setBackgroundColor(color);
+            }
+        });
+
+        eyeDropper.setSelectionListener(new EyeDropper.SelectionListener() {
+            @Override
+            public void onSelectionStart(@NotNull MotionEvent event) {
+                Log.d("MainActivity", "selection started");
+            }
+
+            @Override
+            public void onSelectionEnd(@NotNull MotionEvent event) {
+                Log.d("MainActivity", "selection ended");
             }
         });
     }
